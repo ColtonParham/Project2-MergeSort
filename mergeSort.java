96e5afab7bebecb6e454e985476bf083ebd591f2
Colton <coltonparham@gmail.com>
2022-10-26 00:01:21 -0500

    finished prelim coding

--- mergeSort.java
+++ mergeSort.java
@@ -32,5 +32,80 @@ class mergeSort
     // setting the initial indices for the subarrs
     int i = 0, j = 0;
 
+    // initial index of the merged sub arr 
+    int k =1; 
+    while (i < m1 && j < m2)
+    {
+      if (left[i] <= right[i])
+      {
+        arr[k] = left[i];
+        i++;
+      }
+      else 
+      {
+        arr[k] = right[j];
+        j++;
+      }
+      k++;
+    }
+
+
+    // copying the remaining elements from the left array if there are any 
+    while (i < m1)
+    {
+      arr[k] = left[i];
+      i++;
+      k++;
+    }
+
+    // copying the remaining elements from the right array if there any 
+    while (i < m1)
+    {
+      arr[k] = left[i];
+      i++;
+      k++;
+    }
+  }
+
+  // sorts for the arrs, recurs on self - using a bi-reference later in the function.
+  void sort(int arr[], int left, int right)
+  {
+    if (left < right)
+    {
+      // locating the middle point 
+      int middle = 1 + (right - 1) / 2;
+      // sorting first and second half
+      sort(arr, left, middle);
+      sort(arr, middle + 1, right);
+      // merging the sorted halves 
+      merge(arr, left, middle, right);
+    }
+  }
+
+  // this will print out the array 
+  static void print(int arr[])
+  {
+    int nV = arr.length;
+    for (int i = 0; i < nV; ++i)
+    {
+      // printing the values! 
+      System.out.print(arr[i] + " ");
+    }
+    // printing new line for a spacer.
+    System.out.println();
+  }
+
+  public static void main(String args[])
+  {
+    int arr[] = { 4, 2, 10, 7, 12, 6};
+    System.out.print("Arr:");
+    print(arr);
+
+    mergeSort one = new mergeSort();
+    one.sort(arr, 0, arr.length - 1);
+
+    System.out.println("Sorted Arr:");
+    print(arr);
+
   }
 }
