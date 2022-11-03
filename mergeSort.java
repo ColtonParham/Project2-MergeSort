01503ea9a456b5f12a49f43903554e3619fc6087
Colton <coltonparham@gmail.com>
2022-11-02 20:29:50 -0500

    added 10M values to file

--- mergeSort.java
+++ mergeSort.java
@@ -1,6 +1,9 @@
-
+// Colton Parham: CDP210001 - Project 2, mergeSort.java
+// CS 3345.505, Dr. Zhao
 
 // scanner for input tests, as well as the random, for the random values to be produced inside of the arrays.
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -78,9 +81,8 @@ class mergeSort {
     }
   }
 
-  public static void main(String args[])
+  public static void main(String args[]) throws FileNotFoundException
   {
-    // still need to add a lis of options for what to choose, like 2M from file, hardcoded, as well as randomly generated values 
     // List of Values to be tested
     Scanner input = new Scanner(System.in);
     // input prompt 
@@ -122,7 +124,7 @@ class mergeSort {
         elapsed = endTime - startTime1;
         // Displays the Time it took to run/sort the array
         System.out.print("Took " + elapsed * 0.001 + " Seconds to run and sort through the Array\n");
-        System.out.println("Extra Small Array: 25,000 Elements, Randomly Assorted, and Randomly Generated Values");
+        System.out.println("Extra Small Array: 25,000 Elements(In range from 1-25,000), Randomly Assorted, and Randomly Generated Values");
       break;
 
       // Case 2: Small Array: Randomly Generated Values - Completely Unsorted (50k units)
@@ -150,7 +152,7 @@ class mergeSort {
         elapsed = endTime - startTime2;
         // Displays the Time it took to run/sort the array
         System.out.print("Took " + elapsed * 0.001 + " Seconds to run and sort through the Array\n");
-        System.out.println("Small Array: 50,000 Elements, Randomly Assorted, and Randomly Generated Values");
+        System.out.println("Small Array: 50,000 Elements(In range from 1-50,000), Randomly Assorted, and Randomly Generated Values");
       break;
 
       // Case 3: Medium Small Array: Randomly Generated Values - Completely Unsorted (100k units)
@@ -178,7 +180,7 @@ class mergeSort {
         elapsed = endTime - startTime3;
         // Displays the Time it took to run/sort the array
         System.out.print("Took " + elapsed * 0.001 + " Seconds to run and sort through the Array\n");
-        System.out.println("Medium Small Array: 100,000 Elements, Randomly Assorted, and Randomly Generated Values");
+        System.out.println("Medium Small Array: 100,000 Elements(In range from 1-100,000), Randomly Assorted, and Randomly Generated Values");
       break;
 
       // Case 4: Medium Array: Randomly Generated Values - Completely Unsorted (200k units)
@@ -206,10 +208,92 @@ class mergeSort {
         elapsed = endTime - startTime4;
         // Displays the Time it took to run/sort the array
         System.out.print("Took " + elapsed * 0.001 + " Seconds to run and sort through the Array\n");
-        System.out.println("Medium Array: 200,000 Elements, Randomly Assorted, and Randomly Generated Values");
+        System.out.println("Medium Array: 200,000 Elements(In range from 1-200,000), Randomly Assorted, and Randomly Generated Values");
       break;
-
       
+      // Case 5: Large Array: Randomly Generated Values - Completely Unsorted (500k units)
+      case 5:
+       // Large array - random assortion
+        // having the start time start within the actual case, for most accurate timing.
+        long startTime5 = System.currentTimeMillis();
+        // generating array of 100k random elements
+        int arr5[] = new int[500000];
+        for (int i = 0; i < arr5.length; i++)
+       {
+          // running the random values into the array. 
+          arr5[i] = rd.nextInt();
+       }
+    
+        // object creation for the second object 
+       mergeSort ob5 = new mergeSort(); 
+      
+        // function call for the sort 
+        ob5.sort(arr5, 0, arr5.length - 1); 
+        System.out.println("\nMerge Sorted Array: ");
+
+        // calculation for time it takes to run.
+        endTime = System.currentTimeMillis();
+        elapsed = endTime - startTime5;
+        // Displays the Time it took to run/sort the array
+        System.out.print("Took " + elapsed * 0.001 + " Seconds to run and sort through the Array\n");
+        System.out.println("Large Array: 500,000 Elements(In range from 1-500,000), Randomly Assorted, and Randomly Generated Values");
+      break;
+      
+      //Case 6: Extra Large Array: Randomly Generated Values - Completely Unsorted (1M units)
+      case 6:
+             // Large array - random assortion
+        // having the start time start within the actual case, for most accurate timing.
+        long startTime6 = System.currentTimeMillis();
+        // generating array of 100k random elements
+        int arr6[] = new int[1000000];
+        for (int i = 0; i < arr6.length; i++)
+       {
+          // running the random values into the array. 
+          arr6[i] = rd.nextInt();
+       }
+    
+        // object creation for the second object 
+       mergeSort ob6 = new mergeSort(); 
+      
+        // function call for the sort 
+        ob6.sort(arr6, 0, arr6.length - 1); 
+        System.out.println("\nMerge Sorted Array: ");
+
+        // calculation for time it takes to run.
+        endTime = System.currentTimeMillis();
+        elapsed = endTime - startTime6;
+        // Displays the Time it took to run/sort the array
+        System.out.print("Took " + elapsed * 0.001 + " Seconds to run and sort through the Array\n");
+        System.out.println("Extra Large Array: 1,000,000 Elements(In range from 1-1,000,000), Randomly Assorted, and Randomly Generated Values");
+      break;
+      // Case 7: <FILE>Extra Extra Large Array: Hard-Code Defined Values - Completely Unsorted, and values generated from website, and will be the same across (10M Units)
+      case 7:
+      // Large array - random assortion
+      // having the start time start within the actual case, for most accurate timing.
+      long startTime7 = System.currentTimeMillis();
+      // generating array of 100k random elements
+      Scanner scanner = new Scanner(new File("arr1alt.txt"));
+      int arr7[] = new int[10000000];
+      int i = 0;
+      while(scanner.hasNextInt())
+      {
+        arr7[i++] = scanner.nextInt();
+      }
+
+      // object creation for the second object 
+      mergeSort ob7 = new mergeSort(); 
+
+      // function call for the sort 
+      ob7.sort(arr7, 0, arr7.length - 1); 
+      System.out.println("\nMerge Sorted Array: ");
+
+      // calculation for time it takes to run.
+      endTime = System.currentTimeMillis();
+      elapsed = endTime - startTime7;
+      // Displays the Time it took to run/sort the array
+      System.out.print("Took " + elapsed * 0.001 + " Seconds to run and sort through the Array\n");
+      System.out.println("Extra Extra Large Array: 10,000,000 Elements(In range from 1-10,000,000), Randomly Assorted, and Randomly Generated Values, from txt file");
+      break;
     }
 
     
